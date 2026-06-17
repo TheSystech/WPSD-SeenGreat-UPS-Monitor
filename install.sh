4f6e43a7dde417ce285fb659b65dc562d5468add
Jonathan <jonathan.haase@gmail.com>
2026-06-16 21:29:03 -0500

    Refactor smbus installation check in install.sh
    Check for existing smbus module before installation.

--- install.sh
+++ install.sh
@@ -3,12 +3,17 @@
 mkdir /opt/UPS
 #Copy the python files to the directory
 cp *.py /opt/UPS
-# This will go away eventually
-cp Contrib/*.py /opt/UPS
-#Install the smbus python3 module.
-apt install python3-smbus
+#Check to see if smbus python module is available
+output=$(dpkg -l |grep python3-smbus)
+if [[ "$output" == *"SMBus"* ]]; then
+  echo "SMBus was already installed"
+else
+  #Install the smbus python3 module.
+  apt -y install python3-smbus
+fi
 #copy the systemd file to the correct location
 cp ups-monitor.service /etc/systemd/system/
+cp ups_monitor.conf /etc/
 #Activate the new service on boot and start it now.
 systemctl enable ups-monitor
 systemctl start ups-monitor
